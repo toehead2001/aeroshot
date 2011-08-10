5e54f05d754926c98ae4995d371f220bbd9c62db
comcal116@gmail.com <comcal116@gmail.com@82942b5f-4781-0dfb-f722-c40e932062b4>
2011-08-10 10:19:08 +0000

    Change wording on code file licence info, no functional changes.

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -1,12 +1,12 @@
 ﻿/*  AeroShot - Transparent screenshot utility for Windows
 	Copyright (C) 2011 Caleb Joseph
 
-	This program is free software: you can redistribute it and/or modify
+	AeroShot is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
+	AeroShot is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
