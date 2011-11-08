9cd3cf31072aeeed6a5c1e3a72678ca52beec11e
comcal116@gmail.com <comcal116@gmail.com@82942b5f-4781-0dfb-f722-c40e932062b4>
2011-11-08 12:17:20 +0000

    More cleanup. Merged AssemblyInfo.cs with Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,22 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+
+[assembly: AssemblyTitle("AeroShot")]
+[assembly: AssemblyDescription("Screenshot capture utility for Windows Aero")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AeroShot")]
+[assembly: AssemblyCopyright("GNU GPL v3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0af42042-bc1d-4e23-88ac-23ac05aa2993")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 namespace AeroShot {
 	internal static class Program {
