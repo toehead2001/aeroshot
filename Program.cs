b86a159b1a24a8af2889fa4b9827f9934002076b
comcal116@gmail.com <comcal116@gmail.com@82942b5f-4781-0dfb-f722-c40e932062b4>
2011-12-30 13:32:40 +0000

    Version 1.3

--- Program.cs
+++ Program.cs
@@ -29,8 +29,8 @@ using System.Windows.Forms;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0af42042-bc1d-4e23-88ac-23ac05aa2993")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 namespace AeroShot {
 	internal static class Program {
